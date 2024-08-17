13ead291a01a8ae040c89162005e0304469ab800
JulioOLV <julioccolv@gmail.com>
2024-08-17 13:16:45 -0300

    feat: add migration by umzug

--- src/modules/product-adm/repository/product.model.ts
+++ src/modules/product-adm/repository/product.model.ts
@@ -15,7 +15,7 @@ export default class ProductModel extends Model {
   @Column({ allowNull: false })
   description!: string;
 
-  @Column({ allowNull: false })
+  @Column({ allowNull: true })
   purchasePrice!: number;
 
   @Column({ allowNull: false })
